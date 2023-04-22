71188b97c2e9e1a0d05536123a6f17a6d4e6857a
ap-lodhi <anilpatel.bcs@gmail.com>
2023-04-22 15:05:48 +0530

    Done

--- Quiz/index.js
+++ Quiz/index.js
@@ -38,7 +38,7 @@ var  quizData =[
 ]
 
 var quiz =document.getElementById('quiz')
-var answer = document.querySelectorAll('answer')
+var answer = document.querySelectorAll('.answer')
 var question =document.getElementById('question')
 var option_a = document.getElementById('a_value')
 var option_b = document.getElementById('b_value')
@@ -46,3 +46,43 @@ var option_c = document.getElementById('c_value')
 var option_d = document.getElementById('d_value')
 
 var submitbtn =document.getElementById('submit')
+
+var currQuestion = 0;
+var quizScore = 0;
+
+loadQuiz()
+
+function loadQuiz(){
+    deselect()
+
+    question.innerHTML = quizData[currQuestion].question
+    option_a.innerText =quizData[currQuestion].a
+    option_b.innerText =quizData[currQuestion].b
+    option_c.innerText =quizData[currQuestion].c
+    option_d.innerText =quizData[currQuestion].d
+}
+
+function deselect(){
+    answer.forEach(answer=>answer.checked =false)
+}
+
+submitbtn.addEventListener('click', ()=>{
+    var selectedoption;
+    answer.forEach(answer=>{
+        if(answer.checked){
+            selectedoption =answer.id
+        }
+    })
+
+    if(selectedoption==quizData[currQuestion].correct){
+        quizScore=quizScore+1;
+
+    }
+    currQuestion =currQuestion+1
+   if(currQuestion ==quizData.length){
+
+        document.getElementById('quizdiv').innerHTML=`<h1> you have answerd ${quizScore} correctly out of ${quizData.length}</h1>`    
+   }else{
+    loadQuiz()
+   }
+})
